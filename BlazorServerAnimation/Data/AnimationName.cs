efd27a30f2f6a08223939401e97e6c5e34440389
JustBlazorProgramming <119464091+JustBlazorProgramming@users.noreply.github.com>
2023-02-06 16:53:10 -0500

    Add files via upload

---
+++ BlazorServerAnimation/Data/AnimationName.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorServerAnimation.Data
+{
+    public static class AnimationName
+    {
+        public const string fade = "fade";
+        public const string fadeUp = "fade-up";
+        public const string fadeDown = "fade-down";
+        public const string fadeLeft = "fade-left";
+        public const string fadeRight = "fade-right";
+    }
+}
---
+++ BlazorServerAnimation/wwwroot/js/animation.js
@@ -0,0 +1,3 @@
+﻿window.addEventListener("load", function () {
+    AOS.init();
+});
